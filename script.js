db330b03460c46a8aca005a5540a998f3b3c3596
josuecedeno <josuecedeno@yahoo.com>
2020-09-12 12:39:12 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select a media strewam, pass to video element
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // catch error
+    console.log('whoops, error here:', error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  // Disable button
+  button.disabled = true;
+
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+
+  // Reset button
+  button.disabled = false;
+});
+
+// On load
+selectMediaStream();
